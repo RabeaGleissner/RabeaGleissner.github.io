1ab808561e97c2d2074cad61a23ff3b5d93b0edb
Rabea Gleissner <rabeagleissner@gmail.com>
2016-01-27 13:48:35 +0000

    increased limit

--- public/js/instafeed.js
+++ public/js/instafeed.js
@@ -7,7 +7,7 @@ var userFeed = new Instafeed({
     userId: '20278509',
     resolution: 'standard_resolution',
     accessToken: '20278509.467ede5.a92c1a1ef9964bb0b0abbaa39fe8c038',
-    limit: 15,
+    limit: 40,
     template: '<li><h2 class="day-number"></h2><a href="{{link}}"><img src="{{image}}" /></a><p>{{caption}}</p></li>',
     filter: function(image) {
         return image.tags.indexOf('100days100photos') >= 0;
